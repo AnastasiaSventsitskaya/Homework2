4915d7253f88f5b96fdaac19b9236ff5e9dda49f
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-21 23:17:46 +0400

    выходные

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+Console.WriteLine("Write number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 1 || num > 7)
+{
+    Console.WriteLine("Такого дня недели нет");
+}
+else if (num == 6 || num == 7)
+{
+    Console.WriteLine("Это выходной");   
+}
+else 
+{
+    Console.WriteLine("Это не выходной");
+}
